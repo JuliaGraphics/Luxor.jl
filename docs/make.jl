dd988c4aad0e9faa83cb1083c6ca3edfbd527265
cormullion <cormullion@mac.com>
2016-08-21 16:38:43 +0100

    update CSS

--- docs/make.jl
+++ docs/make.jl
@@ -5,3 +5,6 @@ makedocs(
   format = Documenter.Formats.HTML,
   sitename = "Luxor"
   )
+
+# after this has built the files, they need to be moved up from build/ into docs/ so
+# that they're served at http://cormullion.github.io/Luxor.jl/
