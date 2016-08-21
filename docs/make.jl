1cdf5798cf6467170f0961abf20889c0dc76c246
cormullion <cormullion@mac.com>
2016-08-21 15:08:57 +0100

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,7 @@
 using Documenter, Luxor
 
-#makedocs(modules = [Luxor])
-
 makedocs(
   modules = [Luxor],
   format = Documenter.Formats.HTML,
-  sitename = "Package name"
+  sitename = "Luxor"
   )
