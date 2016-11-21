2e54372347f8ec58d1381fa318b5906fc8d76c3f
cormullion <cormullion@mac.com>
2016-11-21 14:56:21 +0000

    documenter updates

--- docs/make.jl
+++ docs/make.jl
@@ -29,22 +29,24 @@ that they're served at http://cormullion.github.io/Luxor.jl/:
 docs
   build
     assets                ->
-      documenter.css      -> not moved, we've customized it
+      documenter.css      -> moved to docs/assets/
       documenter.js       -> moved to docs/assets/documenter
       search.js           -> moved to docs/assets/search.js
-      style.css           -> not moved, we've customized it
+      style.css           ->  " "
     index.html            -> moved to docs/index.html
     search_index.js       -> moved to docs/index.html
     search.html           -> moved to docs/index.html
 
+You're not supposed to do it this way.
 =#
 
 info("moving files")
 
-#mv("build/assets/documenter.js", "assets/documenter.js", remove_destination=true)
-#mv("build/assets/search.js", "assets/search.js", remove_destination=true)
 #mv("build/assets/logo.png", "assets/logo.png", remove_destination=true)
 
+mv("build/assets/documenter.css", "assets/documenter.css", remove_destination=true)
+mv("build/assets/documenter.js", "assets/documenter.js", remove_destination=true)
+mv("build/assets/search.js", "assets/search.js", remove_destination=true)
 mv("build/search_index.js", "search_index.js", remove_destination=true)
 
 for f in filter(f -> endswith(f, ".html"), readdir("build/"))
