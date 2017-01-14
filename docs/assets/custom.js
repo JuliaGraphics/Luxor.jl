1b0e357fcd006418d5c8e25f2bf6dcd5034320ba
cormullion <cormullion@mac.com>
2017-01-14 10:46:33 +0000

    tidy doc tree

--- docs/assets/custom.js
+++
@@ -1,7 +0,0 @@
-
-var pathname = window.location.pathname;
-if (pathname.indexOf('latest') !== -1) {
-    var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(assets/draft.png)';
-    body.style.backgroundRepeat = 'no-repeat';
-}
