17f05c5ae7b770c832aefbaf1af366639f79d957
cormullion <cormullion@mac.com>
2017-02-11 12:25:24 +0000

    add more methods for sector()

--- test/sector-test.jl
+++ test/sector-test.jl
@@ -17,13 +17,30 @@ setopacity(0.8)
 origin()
 background("black")
 
+# normal
 for inner in 0:20:600
     for a in 0:pi/12:2pi
         randomhue()
-        sector(inner, inner + 20, a, a + pi/12, :fill)
+        sector(O, inner, inner + 20, a, a + pi/12, :fill) 
         sethue("black")
         sector(inner, inner + 20, a, a + pi/12, :stroke)
     end
+end
+
+# rounded
+setopacity(0.75)
+for i in 1:10
+    gsave()
+    translate(rand(-500:500), rand(-500:500))
+    for inner in 90:30:210
+        for a in 0:pi/12:2pi
+            randomhue()
+            sector(O, inner, inner + 20, a, a + pi/12, 15, :fill)
+            sethue("black")
+            sector(inner, inner + 20, a, a + pi/12, 10, :stroke)
+        end
+    end
+    grestore()
 end
 
 @test finish() == true
