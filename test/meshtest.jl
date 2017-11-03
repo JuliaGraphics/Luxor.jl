c974e653685e9c858bc8ea20d02e4f9cc18d9a8a
cormullion <cormullion@mac.com>
2017-11-03 14:13:59 +0000

    bug fix in mesh()

--- test/meshtest.jl
+++ test/meshtest.jl
@@ -12,14 +12,19 @@ end
 function meshtest1(fname)
     Drawing(1200, 1200, fname)
     origin()
-    # transparent, no background
     background("ivory")
-    tiles = Tiler(1200, 1200, 8, 8)
-    setline(0.5)
+    tiles = Partition(1200, 1200, 120, 120)
+    fontsize(120)
+    fontface("Courier-Bold")
     for (pos, n) in tiles
         @layer begin
             translate(pos)
+            setcolor("black")
+            setline(1)
+            box(O, tiles.tilewidth-8, tiles.tileheight-8, :stroke)
+            circle(O, 20, :fill)
             pl = ngon(O, tiles.tilewidth/2, 4, pi/4, vertices=true)
+            # some of these should be transparent
             mesh1 = mesh(pl, [
                 "red",
                 Colors.RGBA(randomcolor()...),
@@ -27,9 +32,8 @@ function meshtest1(fname)
                 Colors.RGBA(randomcolor()...)
                 ])
             setmesh(mesh1)
-            box(O, tiles.tilewidth-8, tiles.tileheight-8, :fillpreserve)
-            sethue("black")
-            box(O, tiles.tilewidth-8, tiles.tileheight-8, :stroke)
+            # box(O, tiles.tilewidth-10, tiles.tileheight-10, :fillpreserve)
+            text(string(Char(rand(64:89))), halign=:center, valign=:middle)
         end
     end
     @test finish() == true
@@ -38,12 +42,14 @@ end
 function meshtest2(fname)
     Drawing(1200, 1200, fname)
     origin()
-    # transparent, no background
     background("ivory")
+    setcolor("black")
+    fontsize(80)
+    text("illuminati", halign=:center)
     pl = ngon(O, 250, 3, pi/6, vertices=true)
     mesh1 = mesh(pl, [
         "purple",
-        "green",
+        Colors.RGBA(randomcolor()...),
         "yellow"
         ])
     setmesh(mesh1)
