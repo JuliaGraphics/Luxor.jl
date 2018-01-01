128b3814fa96bf1de9401d461e82be3ebcfb87b2
cormullion <cormullion@mac.com>
2018-01-01 10:23:34 +0000

    fix bars yheight

--- src/bars.jl
+++ src/bars.jl
@@ -1,13 +1,14 @@
 """
     bars(values::AbstractArray;
-            yscale = 100,
-            xwidth = 10,
+            yheight = 200,
+            xwidth = 25,
             labels = true,
             barfunction = f,
             labelfunction = f,
         )
 
-Draw some bars where each bar is the height of a value in the array.
+Draw some bars where each bar is the height of a value in the array. The bars will fit in
+a box `yheight` high (even if there are negative values).
 
 To control the drawing of the text and bars, define functions that process the
 end points:
@@ -19,8 +20,8 @@ end points:
 and pass them like this:
 
 ```julia
-bars(v, yscale=10, xwidth=10, barfunction=mybarfunction)
-bars(v, xwidth=15, yscale=10, labelfunction=mylabelfunction)
+bars(v, yheight=10, xwidth=10, barfunction=mybarfunction)
+bars(v, xwidth=15, yheight=10, labelfunction=mylabelfunction)
 ```
 
 To suppress the text labels, use optional keyword `labels=false`.
@@ -45,15 +46,15 @@ function bars(values::AbstractArray;
             end
             text(t, tp, halign=:center, valign=:middle)
             end,
-    yscale = 200,
+    yheight = 200,
     xwidth = 25)
     x = O.x
     mn, mx = extrema(values)
     isapprox(mn, mx, atol=0.00001) && (mx = mn + 100) # better show something than nothing
     for (n, v) in enumerate(values)
         # remember y increases downwards by default
-        bottom = Point(x, -rescale(min(v, 0) + mn, mn, mx, 0, yscale))
-        top    = Point(x, -rescale(max(v, 0) + mn, mn, mx, 0, yscale))
+        bottom = Point(x, -rescale(min(v, 0), min(0, mn), mx, 0, yheight))
+        top    = Point(x, -rescale(max(v, 0), min(0, mn), mx, 0, yheight))
         barfunction(bottom, top, v, extremes=extrema(values), barnumber=n, bartotal=length(values))
         labels && labelfunction(bottom, top, v, extremes=extrema(values), barnumber=n, bartotal=length(values))
         x += xwidth
