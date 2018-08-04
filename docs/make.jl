ce4106043fe7bfc389c457f6fd428ac2335489a4
cormullion <cormullion@mac.com>
2018-08-04 13:37:41 +0100

    get docs to build on Linux since Cairo is broken on macOS

--- docs/make.jl
+++ docs/make.jl
@@ -27,8 +27,8 @@ makedocs(
 deploydocs(
     repo = "github.com/JuliaGraphics/Luxor.jl.git",
     target = "build",
-    julia  = "nightly",
-    osname = "osx",
+    julia  = "0.7",
+    osname = "linux",
     deps = nothing,
     make = nothing
 )
