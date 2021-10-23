e443a037f7f78e2fe6b6c7d2fa76cd7a4b975c73
hustf <hustf@users.noreply.github.com>
2021-10-23 20:26:26 +0200

    modified:   src/BoundingBox.jl                    Assume start point at zero, not at center of bbox modified:   test/pointcrossesboundingboxtests.jl  Add perpendicual directions, increase canvas

--- test/pointcrossesboundingboxtests.jl
+++ test/pointcrossesboundingboxtests.jl
@@ -5,31 +5,35 @@ using Luxor
 using Test
 
 function test_lux_bb(;cent = O)
-    bx = BoundingBox(box(O + cent, 100, 100, :none))
+    bx = BoundingBox(box(O + cent, 200, 200, :none))
     box(bx, :stroke)
-    for i in 1:10
-        pt = randompoint((1.5bx)...)
-        pt2 = pointcrossesboundingbox(pt, bx)
-        sethue("grey50")
+    for i in 0:14
+        pt = if i < 4
+            sethue("yellow")
+            ang = i * π / 2
+            O + 40 .* (cos(ang), sin(ang))
+        else
+            sethue("grey50")
+            randompoint((1.5bx)...)
+        end
         arrow(O, pt)
-        sethue("red")
+        pt2 = pointcrossesboundingbox(pt, bx)
         circle(pt2, 3, :stroke)
     end
     snapshot()
 end
 
-function test_pointcrossesbouningbox(fname)
-    Drawing(400,200,:rec)
+function test_pointcrossesboundingbox(fname)
+    Drawing(800,400,:rec)
     background("brown")
-    origin(100,100)
+    origin(200,200)
     test_lux_bb()
-    translate(200,0)
+    translate(400,0)
     test_lux_bb(;cent = (-45, -25))
-    #@test
     snapshot(;fname)
     println("...finished point crosses bounding box test, saved in $(fname)")
 end
 
 fname = "point-crosses-bounding-box-test.png"
 
-test_pointcrossesbouningbox(fname)
+test_pointcrossesboundingbox(fname)
