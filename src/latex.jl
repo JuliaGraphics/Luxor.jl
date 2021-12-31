e4c6b49aa49ad8d033a44fc1b7316bae5f2e2aa5
Davi Barreira <davisbarreira@gmail.com>
2021-12-31 19:08:18 -0300

    Adjusted rotation and created more helper functions for cleaner code.

--- src/latex.jl
+++ src/latex.jl
@@ -1,21 +1,38 @@
 """
-    latextextsize(lstr::LaTeXString)
-Returns the width and height of a latex string.
+    texalign(halign, valign, textw, texth, font_size)
+Helper function to align LaTeX text properly. Returns
+`translate_x` and `translate_y` which consists of the amount
+to be shifted depending on the type of alignment chosen
+and the dimensions of the text.
 """
-function latextextsize(lstr::LaTeXString)
-    bottom_pt, top_pt = latexboundingbox(lstr)
-    textw = top_pt[1] - bottom_pt[1]
-    texth = -(top_pt[2] - bottom_pt[2])
-    return textw, texth
+function texalign(halign, valign, textw, texth, font_size)
+    translate_x, translate_y = 0, 0
+    if halign === :left
+        translate_x = 0
+    elseif halign === :right
+        translate_x = -textw * font_size
+    elseif halign === :center
+        translate_x = -textw / 2 * font_size
+    end
+
+    if valign === :baseline
+        translate_y = 0
+    elseif valign === :bottom
+        translate_y = -texth * font_size
+    elseif valign === :top
+        translate_y = texth * font_size
+    elseif valign === :middle
+        translate_y = texth * font_size / 2
+    end
+    return translate_x, translate_y
 end
 
 """
-    latexboundingbox(lstr::LaTeXString, font_size=1)
-Returns the bounding box containing the latex text with
-`(Lower Left Point, Upper Right Point)`.
-Use `box(latex_bb(testext)...,:stroke)` to draw the bounding box.
+    rawlatexboundingbox(lstr::LaTeXString, font_size=1)
+Helper function that returns the coordinate points
+of the bounding box containing the specific LaTeX text.
 """
-function latexboundingbox(lstr::LaTeXString, font_size=1)
+function rawlatexboundingbox(lstr::LaTeXString)
     sentence = generate_tex_elements(lstr)
     els = filter(x -> x[1] isa TeXChar, sentence)
     texchars = [x[1] for x in els]
@@ -26,18 +43,49 @@ function latexboundingbox(lstr::LaTeXString, font_size=1)
     bottom = bottominkbound.(texchars)
     bottom_pt = Point(0, maximum(-bottom .* scales .- pos_y))
     top_pt = Point(textw, -maximum(inkheight.(texchars) .* scales .+ pos_y .+ bottom))
-    return (bottom_pt * font_size, top_pt * font_size)
+
+    return (bottom_pt, top_pt)
 end
 
+
 """
-    Luxor.text(lstr::LaTeXString, valign=:baseline, halign=:left; kwargs...)
+    latexboundingbox(lstr::LaTeXString, font_size=1)
+Returns the bounding box containing the latex text with
+`(Lower Left Point, Upper Right Point)`.
+Use `box(latex_bb(testext)...,:stroke)` to draw the bounding box.
+"""
+function latexboundingbox(lstr::LaTeXString, font_size=1; halign=:left, valign=:right)
+    bottom_pt, top_pt = rawlatexboundingbox(lstr)
+    textw = top_pt[1] - bottom_pt[1]
+    texth = -(top_pt[2] - bottom_pt[2])
+
+    translate_x, translate_y = texalign(halign, valign, textw, texth, font_size)
+    translate_pt = Point(translate_x,translate_y)
+
+    return (bottom_pt * font_size + translate_pt, top_pt * font_size + translate_pt)
+end
+
+
+"""
+    latextextsize(lstr::LaTeXString)
+Returns the width and height of a latex string.
+"""
+function latextextsize(lstr::LaTeXString)
+    bottom_pt, top_pt = latexboundingbox(lstr)
+    textw = top_pt[1] - bottom_pt[1]
+    texth = -(top_pt[2] - bottom_pt[2])
+    return textw, texth
+end
+
+
+
+"""
+    text(lstr::LaTeXString, pt::Point; valign=:baseline, halign=:left, rotationfixed = false, angle=0, kwargs...)
 Draws LaTeX string using `MathTexEngine.jl`. Hence, uses ModernCMU as font family.
-Note that `valign` is not perfect.
-This function assumes that the axis are in the standard Luxor directions,
-i.e. ↓→.
+When `rotationfixed = true`, the text will rotate around it's own axis, instead of rotating around `pt`.
 """
 function text(
-    lstr::LaTeXString, pt::Point; valign=:baseline, halign=:left, angle=0::Real, kwargs...
+    lstr::LaTeXString, pt::Point; valign=:baseline, halign=:left, angle=0::Real, rotationfixed=false, kwargs...
 )
 
     # Function from MathTexEngine
@@ -48,48 +96,29 @@ function text(
 
     textw, texth = latextextsize(lstr)
 
-    if halign === :left
-        translate_x = 0
-    elseif halign === :right
-        translate_x = -textw
-    elseif halign === :center
-        translate_x = -textw / 2
-    end
-
-    if valign === :baseline
-        translate_y = 0
-    elseif valign === :bottom
-        translate_y = -font_size
-    elseif valign === :top
-        translate_y = texth
-    elseif valign === :middle
-        translate_y = font_size / 2
-    end
+    translate_x, translate_y = texalign(halign, valign, textw, texth, font_size)
 
     # Writes text using ModernCMU font.
     for text in sentence
-        if text[1] isa TeXChar
             @layer begin
+                if !rotationfixed
+                    rotate(angle)
+                end
                 translate(translate_x, translate_y)
                 translate(pt)
-                translate(
-                    (1 - cos(-angle)) * textw / 2 * font_size,
-                    font_size * textw / 2 * sin(-angle),
-                )
-                rotate(angle)
+                if rotationfixed
+                    translate(
+                        (1 - cos(-angle)) * textw / 2 * font_size,
+                        font_size * textw / 2 * sin(-angle),
+                    )
+                    rotate(angle)
+                end
+            if text[1] isa TeXChar
                 fontface(text[1].font.family_name)
                 fontsize(font_size * text[3])
                 Luxor.text(string(text[1].char), Point(text[2]...) * font_size * (1, -1))
-            end
-        elseif text[1] isa HLine
-            @layer begin
-                translate(translate_x, translate_y)
-                translate(pt)
-                translate(
-                    (1 - cos(-angle)) * textw / 2 * font_size,
-                    font_size * textw / 2 * sin(-angle),
-                )
-                rotate(angle)
+
+            elseif text[1] isa HLine
                 pointstart = Point(text[2]...) * font_size * (1, -1)
                 pointend = pointstart + Point(text[1].width, 0) * font_size
                 setline(0.5)
