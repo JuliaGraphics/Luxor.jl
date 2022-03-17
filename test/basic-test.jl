f2426d13a003b7cca7e3303aba6c826733ae0fab
Davi Barreira <davisbarreira@gmail.com>
2022-03-17 14:55:28 -0300

    :tada: New tests for hcat and vcat functions.

--- test/basic-test.jl
+++ test/basic-test.jl
@@ -33,4 +33,23 @@ rp = rand(bb)
     @test distance(p1, c) ≈ distance(p2, c) ≈ distance(p2, c)
 end
 
+@testset "concatenate" begin
+    d1 = Drawing(200,100,:svg)
+    origin()
+    circle(O,60,:fill)
+    finish()
+
+    d2 = Drawing(200,200,:svg)
+    rect(O,200,200,:fill)
+    finish()
+    @test typeof(hcat(d1,d2; hpad=10, valign=:top, clip = true)) <: Drawing
+    @test typeof(hcat(d1,d2; hpad=10, valign=:middle, clip = false)) <: Drawing
+    @test typeof(hcat(d1,d2; hpad=10, valign=:bottom, clip = true)) <: Drawing
+
+    @test typeof(vcat(d1,d2; vpad=10, halign=:left, clip = true)) <: Drawing
+    @test typeof(vcat(d1,d2; vpad=10, halign=:center, clip = false)) <: Drawing
+    @test typeof(vcat(d1,d2; vpad=10, halign=:right, clip = true)) <: Drawing
+
+end
+
 println("...finished basic-test")
