033718f6a1d3b4dd0069f7385221c573dc92402a
cormullion <cormullion@mac.com>
2024-01-14 10:16:24 +0000

    build based on 7b3a4bd

--- versions.js
+++ versions.js
@@ -1,5 +1,12 @@
 var DOC_VERSIONS = [
   "stable",
+  "v3.8",
+  "v3.7",
+  "v3.6",
+  "v3.5",
+  "v3.4",
+  "v3.3",
+  "v3.2",
   "v3.1",
   "v3.0",
   "v2.19",
@@ -39,5 +46,5 @@ var DOC_VERSIONS = [
   "v0.7",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v3.1.1";
+var DOCUMENTER_NEWEST = "v3.8.0";
 var DOCUMENTER_STABLE = "stable";
