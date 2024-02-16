686e457050a0491e3c48e560bb273fa27fb59248
hyrodium <hyrodium@gmail.com>
2024-02-17 01:02:02 +0900

    update `eltype` method

--- src/point.jl
+++ src/point.jl
@@ -51,7 +51,7 @@ Base.getindex(p::Point, i) = (p.x, p.y)[i]
 Base.broadcastable(x::Point) = Ref(x)
 
 # for iteration
-Base.eltype(::Point) = Float64
+Base.eltype(::Type{Point}) = Float64
 Base.iterate(p::Point, state = 1) = state > length(p) ? nothing : (p[state], state + 1)
 Base.length(::Point) = 2
 
