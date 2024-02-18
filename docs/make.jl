6df6d2a77c1d4351dc72ff1317f87aca3d865af0
hyrodium <hyrodium@gmail.com>
2024-02-18 13:40:17 +0900

    fix indents in `docs/make.jl`

--- docs/make.jl
+++ docs/make.jl
@@ -16,57 +16,57 @@ makedocs(
     pages = [
         "Introduction to Luxor" => "index.md",
         "Tutorials" => [
-        "Hello World" => "tutorial/helloworld.md",
-        "Basic path building" => "tutorial/basicpath.md",
-        "Design a logo"       => "tutorial/quickstart.md",
-        "Geometry tutorial"   => "tutorial/basictutorial.md",
-        "Playing with pixels" => "tutorial/pixels.md",
-        "Simple animation"    => "tutorial/simple-animation.md"
+            "Hello World"         => "tutorial/helloworld.md",
+            "Basic path building" => "tutorial/basicpath.md",
+            "Design a logo"       => "tutorial/quickstart.md",
+            "Geometry tutorial"   => "tutorial/basictutorial.md",
+            "Playing with pixels" => "tutorial/pixels.md",
+            "Simple animation"    => "tutorial/simple-animation.md",
         ],
         "Examples" => [
-        "Simple examples" => "example/examples.md",
-        "More examples"   => "example/moreexamples.md",
-        "Gallery"   => "example/gallery.md",
+            "Simple examples" => "example/examples.md",
+            "More examples"   => "example/moreexamples.md",
+            "Gallery"         => "example/gallery.md",
         ],
         "How to guides" => [
-        "Create drawings" => "howto/createdrawings.md",
-        "Draw simple shapes" => "howto/simplegraphics.md",
-        "Use geometry tools" => "howto/geometrytools.md",
-        "Work with tables and grids" => "howto/tables-grids.md",
-        "Use colors and styles" => "howto/colors-styles.md",
-        "Work with polygons" => "howto/polygons.md",
-        "Add text" => "howto/text.md",
-        "Clip graphics" => "howto/clipping.md",
-        "Placing images" => "howto/images.md",
-        "Turtle graphics" => "howto/turtle.md",
-        "Make animations" => "howto/animation.md",
-        "Snapshots" => "howto/snapshots.md",
-        "Interactive graphics and Threads" => "howto/livegraphics.md"
+            "Create drawings"                  => "howto/createdrawings.md",
+            "Draw simple shapes"               => "howto/simplegraphics.md",
+            "Use geometry tools"               => "howto/geometrytools.md",
+            "Work with tables and grids"       => "howto/tables-grids.md",
+            "Use colors and styles"            => "howto/colors-styles.md",
+            "Work with polygons"               => "howto/polygons.md",
+            "Add text"                         => "howto/text.md",
+            "Clip graphics"                    => "howto/clipping.md",
+            "Placing images"                   => "howto/images.md",
+            "Turtle graphics"                  => "howto/turtle.md",
+            "Make animations"                  => "howto/animation.md",
+            "Snapshots"                        => "howto/snapshots.md",
+            "Interactive graphics and Threads" => "howto/livegraphics.md",
         ],
         "Explanations" => [
-        "Basic concepts" => "explanation/basics.md",
-        "Paths vs Polygon" => "explanation/pathspolygons.md",
-        "Perfect pixels and antialising" => "explanation/perfectpixels.md",
-        "Transforms and matrices" => "explanation/transforms.md",
-        "Image matrix" => "explanation/imagematrix.md",
-        "Fonts on Linux" => "explanation/fonts.md",
-        "Luxor and Cairo" => "explanation/luxorcairo.md",
-        "Customize strokepath/fillpath" => "explanation/strokepathdispatch.md",
-        "Contributing" => "explanation/contributing.md"
+            "Basic concepts"                 => "explanation/basics.md",
+            "Paths vs Polygon"               => "explanation/pathspolygons.md",
+            "Perfect pixels and antialising" => "explanation/perfectpixels.md",
+            "Transforms and matrices"        => "explanation/transforms.md",
+            "Image matrix"                   => "explanation/imagematrix.md",
+            "Fonts on Linux"                 => "explanation/fonts.md",
+            "Luxor and Cairo"                => "explanation/luxorcairo.md",
+            "Customize strokepath/fillpath"  => "explanation/strokepathdispatch.md",
+            "Contributing"                   => "explanation/contributing.md",
         ],
         "Reference" => [
-        "Alphabetical function list" => "reference/functionindex.md"
-        "Function reference"         => "reference/api.md"
-        ]
+            "Alphabetical function list" => "reference/functionindex.md",
+            "Function reference"         => "reference/api.md",
+        ],
     ],
 )
 
 repo = "github.com/JuliaGraphics/LuxorManual.git"
 withenv("GITHUB_REPOSITORY" => repo) do
     deploydocs(
-      repo = repo,
-      target = "build",
-      push_preview = true,
-      forcepush = true,
+        repo = repo,
+        target = "build",
+        push_preview = true,
+        forcepush = true,
     )
 end
